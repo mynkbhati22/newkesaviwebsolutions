7f459769851cc85dc95b6cdb2b55df50a3e3fab2
mayank6253 <89696473+mayank6253@users.noreply.github.com>
2022-02-22 18:57:44 +0530

    team cards

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,7 +1,10 @@
-import{ React} from "react";
+import React from "react";
+import director from "./image/director.png";
+import nikhil from "./image/nikhil image.jpg";
+import mustafa from "./image/mustafa-s.jpg";
 import "./Team.css";
 
-function Team() {
+export default function Team() {
   return (
     <>
       <div className="our-team">
@@ -13,62 +16,29 @@ function Team() {
           </p>
         </div>
         <div className="cards d-flex align-items-center justify-content-around">
-          <div className="card" style={{ width: "18rem" }}>
-            <img
-              src="https://d33wubrfki0l68.cloudfront.net/1f2d731d5f5d6c25137b154fcf18a109cb627dcb/2775d/img/nikhil.jpg"
-              className="card-img-top"
-              alt="imglogo"
-            />
+          <div className="card">
+            <img src={director} className="images" alt="imglogo" />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              {/* <a href="/" className="btn btn-primary">
-              Go somewhere
-            </a> */}
+              <h5 className="card-title">Lalita</h5>
+              <p className="card-text">Director</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={nikhil} className="images" alt="imglogo" />
+            <div className="card-body">
+              <h5 className="card-title">Nikhil Rana</h5>
+              <p className="card-text">CEO&CTO</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={mustafa} className="images" alt="imglogo" />
+            <div className="card-body">
+              <h5 className="card-title">Mustafa Siddique</h5>
+              <p className="card-text">Full-Stack Web Developer</p>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="leader-images">
-          <div className="leader-image">
-            <img
-              src="https://d33wubrfki0l68.cloudfront.net/54478b2d20a7aba5bf58fd6612422b7c24a330c1/e51c0/img/lalita.jpg"
-              alt=""
-              className="leader"
-            />
-            <p className="name">
-              Lalita
-              <br />
-            </p>
-          </div>
-          <div className="leader-image">
-            <img
-              src="https://d33wubrfki0l68.cloudfront.net/1f2d731d5f5d6c25137b154fcf18a109cb627dcb/2775d/img/nikhil.jpg"
-              alt=""
-              className="leader "
-            />
-            <p className="name">
-              Nikhil Rana
-              <br />
-            </p>
-          </div>
-          <div className="leader-image">
-            <img
-              src="https://d33wubrfki0l68.cloudfront.net/5d498adfe47b77d68c2775ac91768cdf58f6531c/a2e8b/img/mustafa.jpg"
-              alt=""
-              className="leader"
-            />
-            <p className="name">
-              Mustafa Siddique
-              <br />
-            </p>
-          </div>
-        </div> */}
     </>
   );
 }
-
-export default Team;
